b0f8e39f39e7dd07421dca6db2ee91397ad3ebef
Yasukastsu.A <handbuuuu@gmail.com>
2019-07-09 23:59:15 +0900

    2.1 HTTPハンドラの活用

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func main() {
 	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
 	flag.Parse() // フラグを解釈
 	r := newRoom()
-	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/room", r)
 	// チャットルームを開始
 	go r.run()
