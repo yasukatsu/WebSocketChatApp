db06db26f85649661006a8ce8f88c44bc4071d9a
Yasukastsu.A <handbuuuu@gmail.com>
2019-07-18 22:54:05 +0900

    3.2.1.1 認証サービスを使ったAvatarの実装

--- avatar.go
+++ avatar.go
@@ -13,3 +13,19 @@ type Avatar interface {
 	// 問題が発生した場合にはエラーを、特に、URLを取得できなかった場合にはErrNoAvatarURLを返す
 	GetAvatarURL(c *client) (string, error)
 }
+
+// AuthAvatar ...
+type AuthAvatar struct{}
+
+// UserAuthAvatar ...
+var UserAuthAvatar AuthAvatar
+
+// GetAvatarURL ...
+func (emp AuthAvatar) GetAvatarURL(c *client) (string, error) {
+	if url, ok := c.userData["avatar_url"]; ok {
+		if urlStr, ok := url.(string); ok {
+			return urlStr, nil
+		}
+	}
+	return "", ErrNoAvatarURL
+}
